faa876dc1587a14bd2e6a891cd942deb5025693a
Chrunch-tech <uday014@gmail.com>
2021-05-18 00:51:52 +0530

    Remove unnecessary files and add components and scss folder

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "../scss/App.scss";
+
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
+}
+
+export default App;
